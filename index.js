74840fdd22cc734eb72fe7e164455541b02aea1a
pon32258 <pon1852@hotmail.com>
2018-03-16 09:34:21 +0700

    Upadte google OAUTH

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('/.config/key');
+
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({
-          bye: 'Bye buddy'
-    });
-});
+//clientId 
+// clientSecret 
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientId,
+    clientSecret: keys.googleClientSecret
+}));
 
-const PORT = process.env.PORT || 5000 ; // เป็นการกําหนด PORT ต้องใช่Port ของ Webserver ที่กําหนดให้ เเต่ถ้าก๊สหนดให้เเล้วให้ใช้ Port 5000
+
+const PORT = process.env.PORT || 5000; // เป็นการกําหนด PORT ต้องใช่Port ของ Webserver ที่กําหนดให้ เเต่ถ้าก๊สหนดให้เเล้วให้ใช้ Port 5000
 
 app.listen(PORT);
