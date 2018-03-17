a0154e3eb63b04b4569b4ea8edde36ab33dace81
pon32258 <pon1852@hotmail.com>
2018-03-17 22:08:32 +0700

    Fix Google oAuth

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('/.config/key');
+const keys = require('./config/key');
 
 
 const app = express();
 
-//clientId 
-// clientSecret 
+
 passport.use(new GoogleStrategy({
-    clientID: keys.googleClientId,
-    clientSecret: keys.googleClientSecret
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+    console.log('access token', accessToken);
+    console.log('refresh token', refreshToken);
+    console.log('profile ', profile);
 }));
 
+//เวลาที่เรียก /auth/google ให้ไปเรียก passport.authentication 
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000; // เป็นการกําหนด PORT ต้องใช่Port ของ Webserver ที่กําหนดให้ เเต่ถ้าก๊สหนดให้เเล้วให้ใช้ Port 5000
 
