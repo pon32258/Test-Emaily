20ab6b4661f8cd393fe6101d30c3cfed0aac3c68
pon32258 <pon1852@hotmail.com>
2018-03-19 23:09:44 +0700

    Add asynch and refactor promise object

--- services/passport.js
+++ services/passport.js
@@ -5,34 +5,36 @@ const keys = require('../config/key');
 
 const User = mongoose.model('users'); //มาจาก mongoose.model('users', userSchema); ใน class User.js
 
-passport.serializeUser((user,done)=>{
+passport.serializeUser((user, done) => {
 
-    done(null,user.id);
+    done(null, user.id);
 })
 
-passport.deserializeUser((id, done)=>{
+passport.deserializeUser((id, done) => {
     User.findById(id)
-    .then(user=>{
-        done(null, user);
-    });
+        .then(user => {
+            done(null, user);
+        });
 });
 
 
 passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback',
-    proxy:true
-}, (accessToken, refreshToken, profile, done) => {
-    User.findOne({googleId: profile.Id}).then((existingUser) => {
-            if (existingUser) {
-                done(null,existingUser);
-            } else {
-                new User({googleId: profile.id})
-                .save()
-                .then(user =>done(null,user));
-            }
-        })
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback',
+        proxy: true
+    },
+
+    async (accessToken, refreshToken, profile, done) => {
+        const existingUser = await User.findOne({googleId: profile.Id});
+
+        if (existingUser) {
+            return done(null, existingUser);
+        } 
+            const user = await new User({googleId: profile.id}).save()
+            done(null,user);
+        }
+    )
 
 
-}));
+);
