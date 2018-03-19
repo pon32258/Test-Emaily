135bd9310c4dc2abe6ca490feb5a9312a64be76a
pon32258 <pon1852@hotmail.com>
2018-03-19 21:35:23 +0700

    Prepare production emaily

---
+++ config/prod.js
@@ -0,0 +1,10 @@
+//prod.js - production keys here!!
+
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID ,
+    googleClientSecret: process.env.GOOGLE_SECRET ,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY
+};
+
+//mongodb://pon32258:022102856@ds117729.mlab.com:17729/emaily-prod
