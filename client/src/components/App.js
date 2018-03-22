7b4f877fae06f111d712ff4a970c614887647316
pon32258 <pon1852@hotmail.com>
2018-03-22 21:52:28 +0700

    Redux and Component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { BrowserRouter , Route } from 'react-router-dom';
+
+const Header = () => <h2>Header</h2>
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveyNew = () => <h2>SurwayNew</h2>
+const Landing = () => <h2>Landing</h2>
+
 
 const App = () =>{
     return(
         <div>
-            Hi There!
+            <BrowserRouter>
+                <div> 
+                    <Header />
+                    <Route exact ={true} path="/" component={Landing} />
+                    <Route exact path="/surveys" component={Dashboard}/>
+                    <Route path="/surveys/new" component={SurveyNew}/>
+                </div>
+            </BrowserRouter>
         </div>
     );
 };
 
 export default App;
+
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/App';
+import reducers from './reducers';
 
-ReactDom.render(<App/>,document.querySelector('#root'))
+
+const store = createStore(() => reducers , {}, applyMiddleware())
+
+ReactDom.render(
+    <Provider store = {store}><App /></Provider>,
+    document.querySelector('#root')
+);
